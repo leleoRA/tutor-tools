17a850f17ac9c87ecae748d2ad6dd7e69335d7af
Guga <contato.gustavobarbosa@outlook.com>
2021-08-04 15:59:46 -0300

    code format

--- src/index.js
+++ src/index.js
@@ -16,56 +16,58 @@ async function main() {
     },
   };
 
-  await Promise.all(repositoriesList.map(async (repoURL) => {
-    const { username, repoName } = getRepoInfs(repoURL);
+  await Promise.all(
+    repositoriesList.map(async (repoURL) => {
+      const { username, repoName } = getRepoInfs(repoURL);
 
-    console.log(
-      `Iniciando Fork do repositório ${repoName} do usuário ${username}...`
-    );
-
-    try {
-      const fork = await axios.post(
-        `https://api.github.com/repos/${username}/${repoName}/forks`,
-        body,
-        config
+      console.log(
+        `Iniciando Fork do repositório ${repoName} do usuário ${username}...`
       );
 
-      const forkName = fork.data.name;
-      const folderName = username + "-" + forkName;
+      try {
+        const fork = await axios.post(
+          `https://api.github.com/repos/${username}/${repoName}/forks`,
+          body,
+          config
+        );
 
-      shell.cd("temp")
-      shell.mkdir(folderName);
-      shell.cd(folderName);
+        const forkName = fork.data.name;
+        const folderName = username + "-" + forkName;
 
-      console.log(`Iniciando Clone do repositório ${forkName}...`);
+        shell.cd("temp");
+        shell.mkdir(folderName);
+        shell.cd(folderName);
 
-      shell.exec(
-        `git clone https://github.com/${process.env.GIT_NAME}/${forkName}`
-      );
+        console.log(`Iniciando Clone do repositório ${forkName}...`);
 
-      const deleteIgnore = ["node_modules", "package.lock.json"];
+        shell.exec(
+          `git clone https://github.com/${process.env.GIT_NAME}/${forkName}`
+        );
 
-      console.log(`Iniciando remoção dos arquivos...`);
+        const deleteIgnore = ["node_modules", "package.lock.json"];
 
-      shell.ls(forkName).forEach((item) => {
-        if (!deleteIgnore.includes(item))
-          shell.rm("-rf", forkName + "/" + item);
-      });
+        console.log(`Iniciando remoção dos arquivos...`);
 
-      shell.cd(forkName);
+        shell.ls(forkName).forEach((item) => {
+          if (!deleteIgnore.includes(item))
+            shell.rm("-rf", forkName + "/" + item);
+        });
 
-      shell.exec("git add .");
-      shell.exec("git commit -m 'Preparando revisão de código'");
-      shell.exec("git push");
+        shell.cd(forkName);
 
-      shell.cd("..");
-      shell.cd("..");
-    } catch (err) {
-      console.log(err);
-    }
-  }));
-  
-  console.log("Removendo diretórios temporários...")
+        shell.exec("git add .");
+        shell.exec("git commit -m 'Preparando revisão de código'");
+        shell.exec("git push");
+
+        shell.cd("..");
+        shell.cd("..");
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+
+  console.log("Removendo diretórios temporários...");
   shell.rm("-rf", "*");
 }
 
