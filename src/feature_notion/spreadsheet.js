9f3b5c68749131562d6b9ef3b2892250a658dc95
leleo <celso@respondeai.com.br>
2021-08-20 16:37:31 -0300

    Adicionando busca nas notas

--- src/feature_notion/spreadsheet.js
+++ src/feature_notion/spreadsheet.js
@@ -3,8 +3,13 @@ import creds from '../../client_secret.js';
 
 const spreadsheetId = '1CMrGiaLQ8c8P8HxQF0dzn8nGKN1uiy2Dj_645KX_IpY'
 const sheetTitle = 'Sing me a song';
-const rangeRequisits = 'E2:J2';
-const rangeBonus = 'M2:R2';
+// const rangeRequisits = 'E2:J2';
+// const rangeBonus = 'M2:R2';
+const initialColumnRequisit = 4;
+const endColumnRequisit = 10;
+const rowRequisit = 1;
+
+const requisitsProject = []
 
 async function main(){
   var doc = new GoogleSpreadsheet(spreadsheetId);
@@ -14,11 +19,19 @@ async function main(){
   await doc.loadInfo();
 
   const sheet = doc.sheetsByTitle[sheetTitle]; 
-  await sheet.loadCells();
+  
+  await sheet.loadCells({startColumnIndex:initialColumnRequisit,endColumnIndex:endColumnRequisit});
 
-  const requisitsProject = sheet.getCellByA1(rangeRequisits);
-
+  for (let i = initialColumnRequisit; i < endColumnRequisit; i++) {
+    const requisit = sheet.getCell(rowRequisit,i);
+    requisitsProject.push({
+      "description":requisit.value,
+      "note":requisit.note
+    });
+  }
   console.log(requisitsProject)
+  // const requisitsProject = sheet.getCellByA1(rangeRequisits);
 
+  return true;
 }
 main()
