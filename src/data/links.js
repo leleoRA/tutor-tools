8cb9e2dd35f663098a91a05aa004beabb8554d54
leleo <celso@respondeai.com.br>
2021-09-28 11:18:28 -0300

    clear links

--- src/data/links.js
+++ src/data/links.js
@@ -1,7 +1,5 @@
 const repositories= [
-  "https://github.com/camilocoelhogomes/projeto_10_trackit.git",
-  "https://github.com/leandrodcs/trackIt",
-  "https://github.com/andeaquino/Projeto10-TrackIt"
+
 ]
 
 export default repositories;
