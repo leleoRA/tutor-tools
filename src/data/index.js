32f8673557ed927d9caeb1fad172ff4e7d893372
thiagoribb <thiribeiro142@gmail.com>
2021-10-04 16:15:03 -0300

    feat: initial data structure

---
+++ src/data/index.js
@@ -0,0 +1,87 @@
+const data = [
+  {
+    turma: 3,
+    modulos: [
+      {
+        id: 1,
+        nome: "Front-end com interfaces estáticas",
+        link: "www.google.com",
+        projetos: [
+          {
+            nome: "Globo.com",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 1
+          },
+          {
+            nome: "Instagram",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 2
+          }
+        ]
+      },
+      {
+        id: 2,
+        nome: "Front-end com interfaces dinâmicas",
+        link: "www.google.com",
+        projetos: [
+          {
+            nome: "Foodcamp",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 3
+          },
+          {
+            nome: "Parrot Card Game",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 4
+          },
+          {
+            nome: "Bate-Papo Uol",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 5
+          }
+        ]
+      }]
+  },
+  {
+    turma: 4,
+    modulos: [
+      {
+        id: 1,
+        nome: "Modelagem e Arquitetura",
+        link: "www.google.com",
+        projetos: [
+          {
+            nome: "globo.com",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 17,
+          },
+          {
+            nome: "globo.com",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 17,
+          }
+        ]
+      },
+      {
+        id: 2,
+        link: "www.google.com",
+        projetos: [
+          {
+            nome: "globo.com",
+            qtddRepos: 1,
+            colunasDeCriterios: ["E", "J"],
+            semana: 17,
+          }
+        ]
+      }]
+  }
+];
+
+export default data;
