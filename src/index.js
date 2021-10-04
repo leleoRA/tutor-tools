53a7a914ca52a78fddba1dfeda86fd7db749e381
thiagoribb <thiribeiro142@gmail.com>
2021-10-04 18:47:20 -0300

    feat: ask class and module

--- src/index.js
+++ src/index.js
@@ -10,10 +10,48 @@ import * as communicationController from "./controllers/communication.js";
 import * as deliveryReviewController from "./controllers/deliveryReview.js";
 
 import * as hooks from "./utils/hooks/index.js";
+import data from "./data/index.js";
 
 global.root = shell.pwd().stdout;
 
 async function main() {
+  askInformation();
+}
+
+function askInformation() {
+  const classes = data.map((classInfo) => {
+    return "Turma " + classInfo.turma;
+  });
+  console.log(classes);
+
+  const index = readlineSync.keyInSelect(
+    classes,
+    chalk.bold("Olá! Bem-vindo ao Tutor-Tools! Em qual turma você trabalha atualmente?")
+  );
+
+  switch (index + 1) {
+    case 1:
+      askModule();
+      break;
+
+    case 2:
+      break;
+
+  }
+}
+
+function askModule() {
+  const modules = data[1].modulos.map((modulo) => {
+    return "Modulo " + modulo.id;
+  });
+
+  const index = readlineSync.keyInSelect(
+    modules,
+    chalk.bold("O projeto que você deseja avaliar faz parte de qual módulo?")
+  );
+}
+
+async function askOperation() {
   const operations = [
     "Revisão de Entrega",
     "Revisão de Código",
