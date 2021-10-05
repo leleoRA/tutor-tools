a9d21365af97e4369c175a8b96fe9303e12249e1
thiagoribb <thiribeiro142@gmail.com>
2021-10-05 19:03:51 -0300

    refactor: main and userInteraction

--- src/utils/userInteraction/index.js
+++ src/utils/userInteraction/index.js
@@ -4,6 +4,12 @@ import chalk from 'chalk'
 import userInputValidation from '../userInputValidation/index.js'
 import classData from '../../data/index.js'
 
+import * as gitHubAuth from '../../auth/gitHubTokenAuth.js'
+import * as codeReviewController from '../../controllers/codeReview.js'
+import * as communicationController from '../../controllers/communication.js'
+import * as deliveryReviewController from '../../controllers/deliveryReview.js'
+import * as hooks from '../hooks/index.js'
+
 export function askClass() {
   const classNames = classData.map(
     (classInfo) => `Turma ${classInfo.className}`
@@ -45,4 +51,49 @@ export function askProject(module) {
   userInputValidation(index)
 
   return module.projects[index]
+}
+
+export async function askOperation(projectInfo) {
+  const operations = [
+    'Revisão de Entrega',
+    'Revisão de Código',
+    'Gerar Comunicação',
+    'Finalizar Avaliação',
+  ]
+
+  const index = readlineSync.keyInSelect(
+    operations,
+    chalk.bold('Qual operação deseja realizar?')
+  )
+  const spreadsheetId = '1WggtFYE6R7OY0kFMIAv-IjY-PN4OZ3EJSqmsveWqTjM'
+  const sheetTitle = 'teste'
+  const nSemana = '17'
+  switch (index + 1) {
+    case 1:
+      await deliveryReviewController.prepareReview(spreadsheetId, sheetTitle)
+      break
+
+    case 2:
+      try {
+        await gitHubAuth.authenticate()
+        await codeReviewController.prepareReview(spreadsheetId, sheetTitle)
+      } catch (err) {
+        console.log(err)
+      }
+      break
+
+    case 3:
+      await communicationController.prepareCommunication(
+        spreadsheetId,
+        sheetTitle,
+        nSemana
+      )
+      break
+
+    case 4:
+      hooks.clear()
+      break
+    default:
+      break
+  }
 }
