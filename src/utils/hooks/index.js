27964f1312f6fcf960a0cbae9f0b41e8fee24c71
thiagoribb <thiribeiro142@gmail.com>
2021-10-07 13:01:26 -0300

    refactor: clear function and remove os-name lib

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,5 +1,4 @@
 import shell from 'shelljs'
-import osName from 'os-name'
 
 export function getConfig(token) {
   return {
@@ -12,18 +11,11 @@ export function getConfig(token) {
 export function clear(directoryToClear = null) {
   console.log('Removendo diretórios temporários...')
 
-  // const system = osName()
-  // const separator = system.includes('Windows') ? '\\' : '/'
-
   shell.cd(global.root)
-  // const pathDirectoryList = shell.pwd().split(separator)
-  // const actualDirectory = pathDirectoryList.pop()
-
-  // if (actualDirectory === 'tutor-tools') {
   shell.cd('temp')
   if (directoryToClear) {
     shell.cd(directoryToClear)
   }
+
   shell.rm('-rf', '*')
-  // }
 }
